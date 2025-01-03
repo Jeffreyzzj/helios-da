1d80ae3969968cf54e190d4d83a9813b13c6b5bb
zijie.zeng <jeffreyzzj1996@163.com>
2025-01-03 15:46:06 +0800

    zijie.zeng push code

--- go-helios-da/build.sh
+++ go-helios-da/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 进入 Go 工程目录，假设工程目录为 $HOME/go_project
-cd $HOME/go-helios-da
+# cd $HOME/go-helios-da
 
 # 构建 Linux 可执行文件
 GOOS=linux GOARCH=amd64 go build -o helios-da-linux main.go
@@ -13,9 +13,12 @@ GOOS=windows GOARCH=amd64 go build -o helios-da-windows.exe main.go
 GOOS=darwin GOARCH=amd64 go build -o helios-da-macos main.go
 
 # 检查 da_conf 文件是否存在
-if [ -f "da_conf" ]; then
+if [ -d "./da_conf" ]; then
     # 打包成 tar.gz
     tar -czvf helios-da.tar.gz helios-da-linux helios-da-windows.exe helios-da-macos da_conf
+    rm helios-da-linux
+    rm helios-da-windows.exe
+    rm helios-da-macos
 else
     echo "da_conf file not found."
     exit 1
